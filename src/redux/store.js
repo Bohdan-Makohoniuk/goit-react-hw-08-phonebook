8660ad5300c1dd055474caf1ef859d1e493e1d11
Bohdan <103361804+Bohdan-Makohoniuk@users.noreply.github.com>
2023-02-11 19:13:08 +0200

    filter

--- src/redux/store.js
+++ src/redux/store.js
@@ -12,7 +12,7 @@ import {
 import storage from 'redux-persist/lib/storage';
 import { authReducer } from './auth/slice';
 import { contactsReducer } from './contacts/contactsSlice';
-import { filterReducer } from './contacts/contactsSlise';
+import { filterReducer } from './contacts/filterSlice';
 
 const persistConfig = {
   key: 'auth',
